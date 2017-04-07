19be875561d8986f178e6e4995168e99c33a0183
phamduyly <s3594800@rmit.edu.vn>
2017-04-07 15:13:25 +0700

    Commit

--- Task-05.04/Task-05.04/frmProduct.vb
+++ Task-05.04/Task-05.04/frmProduct.vb
@@ -70,4 +70,8 @@ Public Class frmProduct
         chkDiscontinued.Checked = False
 
     End Sub
+
+    Private Sub frmProduct_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
