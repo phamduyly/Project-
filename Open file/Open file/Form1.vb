78f2bfe676e6aa73931b67416d4fb4646565cab4
phamduyly <s3594800@rmit.edu.vn>
2017-04-07 13:52:23 +0700

    added about,help framework

--- Open file/Open file/Form1.vb
+++ Open file/Open file/Form1.vb
@@ -5,7 +5,7 @@ Option Explicit On
 Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim sParam As String = "C:\Users\s3594800\Source\Repos\Project-\index.html"
+        Dim sParam As String = "C:\Users\s3594800\Desktop\Framework for Help, about, document\Blog Template for Bootstrap.html"
         ' the """"" can fix into the access to the file path
         MsgBox("sParam: " & sParam)
 
